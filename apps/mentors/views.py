59324e88168c47109b732feba8fdf44fa0f93cd0
Ulanbekkn <kubanycbekovulanbek7@gmail.com>
2023-06-22 15:01:23 +0600

    .

--- apps/mentors/views.py
+++ apps/mentors/views.py
@@ -18,22 +18,23 @@ class MentorAPIView(ListCreateAPIView):
     pagination_class = PageNumberPagination
     permission_classes = (IsAuthenticated, )
 
-    # def post(self, request, *args, **kwargs):
-    #     serializer = MentorSerializer(data=request.data)
-    #     if not serializer.is_valid():
-    #         return Response(data={"errors": serializer.errors}, status=status.HTTP_406_NOT_ACCEPTABLE)
-    #     username = serializer.validated_data.get('username')
-    #     group = serializer.validated_data.get('group')
-    #     about_me = serializer.validated_data.get('about_me')
-    #     contact = serializer.validated_data.get('contact')
-    #     skills = serializer.validated_data.get('skills')
-    #     employment = serializer.validated_data.get('employment')
-    #     directions = Mentor.objects.filter(data=request.user.directions)
-    #     month = Mentor.objects.filter(data=request.user.month)
-    #     mentor = Mentor.objects.create(username=username, group=group, about_me=about_me, contact=contact, skills=skills,
-    #                                    employment=employment, directions=directions, month=month)
-    #     mentor.skills.set(skills)
-    #     return Response(data=MentorSerializer(mentor).data)
+    def post(self, request, pk, *args, **kwargs):
+        user = User.objects.get(id=pk)
+        serializer = MentorSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(data={"errors": serializer.errors}, status=status.HTTP_406_NOT_ACCEPTABLE)
+        username = serializer.validated_data.get('username')
+        group = serializer.validated_data.get('group')
+        about_me = serializer.validated_data.get('about_me')
+        contact = serializer.validated_data.get('contact')
+        skills = serializer.validated_data.get('skills')
+        employment = serializer.validated_data.get('employment')
+        directions = Mentor.objects.filter(data=request.user.directions)
+        month = Mentor.objects.filter(data=request.user.month)
+        mentor = Mentor.objects.create(username=username, group=group, about_me=about_me, contact=contact, skills=skills,
+                                       employment=employment, directions=directions, month=month)
+        mentor.skills.set(skills)
+        return Response(data=MentorSerializer(mentor).data)
 
 
 class MentorDetailAPIView(RetrieveUpdateDestroyAPIView):
